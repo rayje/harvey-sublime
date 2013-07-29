ed246a169aef247b06d75319276d0d1c033dbb46
Rayland <dev.rayje@gmail.com>
2013-07-28 21:47:06 -0600

    Log highlighted string to display

--- harvey.py
+++ harvey.py
@@ -10,10 +10,29 @@ class HarveyCommand(sublime_plugin.TextCommand):
 		self.window = self.view.window()
 		self.load_config()
 
-		self.display_results()
+		test = self.get_test_name()
+
+		self.run_shell_command('echo "' + test + '"')
 
 	def display_results(self):
 		self.panel = self.window.get_output_panel("exec")
 		self.window.run_command("show_panel", {"panel": "output.exec"})
 		self.panel.settings().set("color_scheme", THEME)
 		self.panel.set_syntax_file(SYNTAX)
+
+	def get_test_name(self):
+		region = self.view.sel()[0]
+		text_string = self.view.substr(region)
+
+		return text_string
+
+	def run_shell_command(self, command):
+		if not command:
+			return False
+
+		self.view.window().run_command("exec", {
+			"cmd": [command],
+			"shell": True
+		})
+
+		self.display_results()
