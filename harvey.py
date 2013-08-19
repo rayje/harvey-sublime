7d2eede2ed3ec1629275c9411acf718cd70cb59e
Rayland <dev.rayje@gmail.com>
2013-08-19 07:51:24 -0600

    Fix add test files to accept an array of strings

--- harvey.py
+++ harvey.py
@@ -136,7 +136,8 @@ class HarveyCommand(sublime_plugin.TextCommand):
 				(node, harvey, config, reporter, test)
 
 		if addTestFiles and addTestFiles != '':
-			command = command + ' --addTestFiles ' + addTestFiles
+			test_files = ",".join(addTestFiles)
+			command = command + ' --addTestFiles ' + test_files
 
 		if test_id:
 			tags = ' --tags "%s"' % test_id
@@ -502,3 +503,34 @@ class HarveyGoToDefinitionCommand(HarveyCommand):
 
 		# Scroll to the line where the definition exists
 		view.show(select_region)
+
+# class AutoCompleteCommand(sublime_plugin.EventListener):
+
+#     def on_query_completions(self, view, prefix, locations):
+#     	print 'on_query_completions'
+#         window = sublime.active_window()
+#         views = window.views()
+#         print len(views)
+#         print '-', view.buffer_id()
+#         for v in views:
+#         	print v.buffer_id()
+#         print 'pre:', prefix
+#         print 'sel.a:', view.sel()[0].a
+
+#         # get results from each tab
+#         results = [v.extract_completions(prefix) for v in window.views() if v.buffer_id() != view.buffer_id()]
+#         print results
+#         results = [(item,item) for sublist in results for item in sublist] #flatten
+#         results = list(set(results)) # make unique
+#         results.sort() # sort
+#         return results
+
+	# def on_post_save(self, view):
+	# 	import inspect
+	# 	print 'EventListener'
+	# 	print inspect.getmembers(sublime, predicate=inspect.isfunction)
+
+# class HarveyTest(HarveyCommand):
+# 	def run(self, edit):
+# 		import inspect
+# 		print inspect.getdoc(sublime.View)
